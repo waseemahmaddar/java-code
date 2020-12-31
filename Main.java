1e4f809fee124f703e83aa8c56f91effb1648e39
waseemahmaddar <48276326+waseemahmaddar@users.noreply.github.com>
2020-12-31 14:48:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main{
+	public static void main(String[] args) {
+
+		int day = 4;
+
+		switch(day){
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("WED");
+				break;
+			case 4:
+				System.out.println("THU");
+				break;
+			case 5:
+				System.out.println("FRI");
+				break;
+			case 6:
+				System.out.println("SAT");
+				break;
+			case 7:
+				System.out.println("SUN");
+				break;
+			default:
+				System.out.println("The day is invalid");
+
+		}
+		
+	}
+}
